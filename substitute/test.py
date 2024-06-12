cc801ea965b544eb8bc965c0ba7af5bd19fbe200
COSSEVIN Erwan <cossevine@pxdesrtls020>
2024-06-12 10:49:58 +0200

    add bugs

--- substitute/test.py
+++ substitute/test.py
@@ -1,29 +1,22 @@
-from loki import *
-#src_file = "sub.F90"
-#src_name = "TEST"
-src_file = "ppwetpoint.F90"
+from loki import Sourcefile
+from loki import fgen
+from loki import resolve_associates
+import logical
+import sys
+print(len(sys.argv))
+import click
+print(len(sys.argv))
+
+true_symbols = []
+false_symbols = ['LHOOK']
+
+file_name = "ppwetpoint_bug_test.F90"
+#file_name = "ppwetpoint_no_bug.F90"
+#file_name = "ppwetpoint_bug.F90"
+source=Sourcefile.from_file(file_name)
 src_name = "PPWETPOINT"
-
-source = Sourcefile.from_file(src_file)
-routine = source[src_name]
-
-loop_index = routine.variable_map['JLON']
-variables = [var for var in FindVariables(unique=True).visit(routine.spec)]
-for var in variables:
-    if var.name == 'JLON':
-        loop_index2 = var
-
-variables = [var for var in FindVariables().visit(routine.body)]
-for var in variables:
-    if var.name == 'JLON':
-        loop_index3 = var
-
-print("test =", loop_index is loop_index2)
-print("test =", loop_index is loop_index3)
-print("test =", loop_index2 is loop_index3)
-
-rename_map = {loop_index3 : loop_index2}
-
-routine.body = SubstituteExpressions(rename_map).visit(routine.body)
-
-print(fgen(routine.body))
+routine=source[src_name]
+fgen(routine)
+resolve_associates(routine)
+logical.transform_subroutine(routine, true_symbols, false_symbols)
+routine_t1 = routine.clone()
